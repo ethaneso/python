b78f8b5eb38094c215e0aa9b8c3cfaa8c8ad6cfa
ethankclam <ethankclam@gmail.com>
2018-09-01 01:36:31 +0800

    calculators

---
+++ calculators.py
@@ -0,0 +1,20 @@
+# Addition 
+print(5 + 5)
+
+# subtraction
+print(5 - 5)
+
+# Multiplication
+print(3 * 5)
+
+# Division
+print(10 / 2)
+
+# Modulo
+print(18 % 7)
+
+# Exponentiation
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years?
+print(100*1.1**7)
