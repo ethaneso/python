037772b60a4a570262c0b84d12cf6a5a0cda089d
ethankclam <ethankclam@gmail.com>
2018-09-01 01:39:23 +0800

    variables

---
+++ variable.py
@@ -0,0 +1,11 @@
+# Create a variable savings
+savings = 100
+
+# Create a variable factor
+factor = 1.10
+power  = 7
+# Calculate result
+result = savings*factor**power
+
+# Print out result
+print(result)
