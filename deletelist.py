be5e6444e1ad2a08611fbcf8301716c777abdec7
ethankclam <ethankclam@gmail.com>
2018-09-01 03:05:18 +0800

    delete list element

---
+++ deletelist.py
@@ -0,0 +1,8 @@
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+        "bathroom", 10.50, "poolhouse", 24.5,
+        "garage", 15.45]
+
+del(areas[10:11])
+
+print(areas)
