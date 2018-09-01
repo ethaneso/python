2d39db3c1f64e21ba20496b1ec8fdf05d6fce537
ethankclam <ethankclam@gmail.com>
2018-09-01 10:38:24 +0800

    functions: type(), len(), int(), str()

---
+++ functions.py
@@ -0,0 +1,18 @@
+# Create variables var1 and var2
+var1 = [1, 2, 3, 4, 5.84]
+var2 = False
+
+# Print out type of var1
+print(type(var1))
+
+# Print out length of var1
+print(len(var1))
+
+# Convert var2 to an integer: out2
+out2 = int(var2)
+
+# Print out type of out2
+print(type(out2))
+
+#print out after coverting var2 to string
+print(str(var2))
